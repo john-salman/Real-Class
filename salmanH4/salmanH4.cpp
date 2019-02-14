b56d6e5e9d5aec03e8ada2096109e3484a28282b
John Salman <31483839+john-salman@users.noreply.github.com>
2019-02-13 17:25:52 -0800

    Testing file added
    Dependent on an input file, formatted like "test.txt".

--- salmanH4/salmanH4.cpp
+++ salmanH4/salmanH4.cpp
@@ -1,151 +1,63 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include "Real.h"
 
 using namespace std;
 
-int main() {
-  Real double_1 (432.875);
-  Real double_2 (0.87576473874);
-  Real double_3 (57485839.2394348);
-  Real double_4 (0.0);
-  Real double_5 (34590);
-
-  Real double_1_neg (-432.875);
-  Real double_2_neg (-0.8757673874);
-  Real double_3_neg (-57485839.2394348);
-  Real double_4_neg (-0.0);
-  Real double_5_neg (-34590);
-
-  string TF [2] = {"false", "true"};
-  
-  cout << endl;
-  cout << double_1 << " > " << double_1 << " = " << TF[double_1 > double_1] << endl;
-  cout << double_1 << " > " << double_2 << " = " << TF[double_1 > double_2] << endl;
-  cout << double_1 << " > " << double_3 << " = " << TF[double_1 > double_3] << endl;  
-  cout << double_1 << " > " << double_4 << " = " << TF[double_1 > double_4] << endl;
-  cout << double_1 << " > " << double_5 << " = " << TF[double_1 > double_5] << endl;
-									   
-  cout << double_1 << " > " << double_1_neg << " = " << TF[double_1 > double_1_neg] << endl;
-  cout << double_1 << " > " << double_2_neg << " = " << TF[double_1 > double_2_neg] << endl;
-  cout << double_1 << " > " << double_3_neg << " = " << TF[double_1 > double_3_neg] << endl;
-  cout << double_1 << " > " << double_4_neg << " = " << TF[double_1 > double_4_neg] << endl;
-  cout << double_1 << " > " << double_5_neg << " = " << TF[double_1 > double_5_neg] << endl;
-
-  cout << endl;
-  cout << double_2 << " > " << double_1 << " = " << TF[double_2 > double_1] << endl;
-  cout << double_2 << " > " << double_2 << " = " << TF[double_2 > double_2] << endl;
-  cout << double_2 << " > " << double_3 << " = " << TF[double_2 > double_3] << endl;
-  cout << double_2 << " > " << double_4 << " = " << TF[double_2 > double_4] << endl;
-  cout << double_2 << " > " << double_5 << " = " << TF[double_2 > double_5] << endl;
-
-  cout << double_2 << " > " << double_1_neg << " = " << TF[double_2 > double_1_neg] << endl;
-  cout << double_2 << " > " << double_2_neg << " = " << TF[double_2 > double_2_neg] << endl;
-  cout << double_2 << " > " << double_3_neg << " = " << TF[double_2 > double_3_neg] << endl;
-  cout << double_2 << " > " << double_4_neg << " = " << TF[double_2 > double_4_neg] << endl;
-  cout << double_2 << " > " << double_5_neg << " = " << TF[double_2 > double_5_neg] << endl;
-
-  cout << endl;
-  cout << double_3 << " > " << double_1 << " = " << TF[double_3 > double_1] << endl;
-  cout << double_3 << " > " << double_2 << " = " << TF[double_3 > double_2] << endl;
-  cout << double_3 << " > " << double_3 << " = " << TF[double_3 > double_3] << endl;
-  cout << double_3 << " > " << double_4 << " = " << TF[double_3 > double_4] << endl;
-  cout << double_3 << " > " << double_5 << " = " << TF[double_3 > double_5] << endl;
-
-  cout << double_3 << " > " << double_1_neg << " = " << TF[double_3 > double_1_neg] << endl;
-  cout << double_3 << " > " << double_2_neg << " = " << TF[double_3 > double_2_neg] << endl;
-  cout << double_3 << " > " << double_3_neg << " = " << TF[double_3 > double_3_neg] << endl;
-  cout << double_3 << " > " << double_4_neg << " = " << TF[double_3 > double_4_neg] << endl;
-  cout << double_3 << " > " << double_5_neg << " = " << TF[double_3 > double_5_neg] << endl;
-
-  cout << endl;
-  cout << double_4 << " > " << double_1 << " = " << TF[double_4 > double_1] << endl;
-  cout << double_4 << " > " << double_2 << " = " << TF[double_4 > double_2] << endl;
-  cout << double_4 << " > " << double_3 << " = " << TF[double_4 > double_3] << endl;
-  cout << double_4 << " > " << double_4 << " = " << TF[double_4 > double_4] << endl;
-  cout << double_4 << " > " << double_5 << " = " << TF[double_4 > double_5] << endl;
-
-  cout << double_4 << " > " << double_1_neg << " = " << TF[double_4 > double_1_neg] << endl;
-  cout << double_4 << " > " << double_2_neg << " = " << TF[double_4 > double_2_neg] << endl;
-  cout << double_4 << " > " << double_3_neg << " = " << TF[double_4 > double_3_neg] << endl;
-  cout << double_4 << " > " << double_4_neg << " = " << TF[double_4 > double_4_neg] << endl;
-  cout << double_4 << " > " << double_5_neg << " = " << TF[double_4 > double_5_neg] << endl;
-
-  cout << endl;
-  cout << double_5 << " > " << double_1 << " = " << TF[double_5 > double_1] << endl;
-  cout << double_5 << " > " << double_2 << " = " << TF[double_5 > double_2] << endl;
-  cout << double_5 << " > " << double_3 << " = " << TF[double_5 > double_3] << endl;
-  cout << double_5 << " > " << double_4 << " = " << TF[double_5 > double_4] << endl;
-  cout << double_5 << " > " << double_5 << " = " << TF[double_5 > double_5] << endl;
-
-  cout << double_5 << " > " << double_1_neg << " = " << TF[double_5 > double_1_neg] << endl;
-  cout << double_5 << " > " << double_2_neg << " = " << TF[double_5 > double_2_neg] << endl;
-  cout << double_5 << " > " << double_3_neg << " = " << TF[double_5 > double_3_neg] << endl;
-  cout << double_5 << " > " << double_4_neg << " = " << TF[double_5 > double_4_neg] << endl;
-  cout << double_5 << " > " << double_5_neg << " = " << TF[double_5 > double_5_neg] << endl;
-
-  cout << endl;
-  cout << double_1_neg << " > " << double_1 << " = " << TF[double_1_neg > double_1] << endl;
-  cout << double_1_neg << " > " << double_2 << " = " << TF[double_1_neg > double_2] << endl;
-  cout << double_1_neg << " > " << double_3 << " = " << TF[double_1_neg > double_3] << endl;
-  cout << double_1_neg << " > " << double_4 << " = " << TF[double_1_neg > double_4] << endl;
-  cout << double_1_neg << " > " << double_5 << " = " << TF[double_1_neg > double_5] << endl;
-
-  cout << double_1_neg << " > " << double_1_neg << " = " << TF[double_1_neg > double_1_neg] << endl;
-  cout << double_1_neg << " > " << double_2_neg << " = " << TF[double_1_neg > double_2_neg] << endl;
-  cout << double_1_neg << " > " << double_3_neg << " = " << TF[double_1_neg > double_3_neg] << endl;
-  cout << double_1_neg << " > " << double_4_neg << " = " << TF[double_1_neg > double_4_neg] << endl;
-  cout << double_1_neg << " > " << double_5_neg << " = " << TF[double_1_neg > double_5_neg] << endl;
-
-  cout << endl;
-  cout << double_2_neg << " > " << double_1 << " = " << TF[double_2_neg > double_1] << endl;
-  cout << double_2_neg << " > " << double_2 << " = " << TF[double_2_neg > double_2] << endl;
-  cout << double_2_neg << " > " << double_3 << " = " << TF[double_2_neg > double_3] << endl;
-  cout << double_2_neg << " > " << double_4 << " = " << TF[double_2_neg > double_4] << endl;
-  cout << double_2_neg << " > " << double_5 << " = " << TF[double_2_neg > double_5] << endl;
-
-  cout << double_2_neg << " > " << double_1_neg << " = " << TF[double_2_neg > double_1_neg] << endl;
-  cout << double_2_neg << " > " << double_2_neg << " = " << TF[double_2_neg > double_2_neg] << endl;
-  cout << double_2_neg << " > " << double_3_neg << " = " << TF[double_2_neg > double_3_neg] << endl;
-  cout << double_2_neg << " > " << double_4_neg << " = " << TF[double_2_neg > double_4_neg] << endl;
-  cout << double_2_neg << " > " << double_5_neg << " = " << TF[double_2_neg > double_5_neg] << endl;
-
-  cout << endl;
-  cout << double_3_neg << " > " << double_1 << " = " << TF[double_3_neg > double_1] << endl;
-  cout << double_3_neg << " > " << double_2 << " = " << TF[double_3_neg > double_2] << endl;
-  cout << double_3_neg << " > " << double_3 << " = " << TF[double_3_neg > double_3] << endl;
-  cout << double_3_neg << " > " << double_4 << " = " << TF[double_3_neg > double_4] << endl;
-  cout << double_3_neg << " > " << double_5 << " = " << TF[double_3_neg > double_5] << endl;
-
-  cout << double_3_neg << " > " << double_1_neg << " = " << TF[double_3_neg > double_1_neg] << endl;
-  cout << double_3_neg << " > " << double_2_neg << " = " << TF[double_3_neg > double_2_neg] << endl;
-  cout << double_3_neg << " > " << double_3_neg << " = " << TF[double_3_neg > double_3_neg] << endl;
-  cout << double_3_neg << " > " << double_4_neg << " = " << TF[double_3_neg > double_4_neg] << endl;
-  cout << double_3_neg << " > " << double_5_neg << " = " << TF[double_3_neg > double_5_neg] << endl;
-
-  cout << endl;
-  cout << double_4_neg << " > " << double_1 << " = " << TF[double_4_neg > double_1] << endl;
-  cout << double_4_neg << " > " << double_2 << " = " << TF[double_4_neg > double_2] << endl;
-  cout << double_4_neg << " > " << double_3 << " = " << TF[double_4_neg > double_3] << endl;
-  cout << double_4_neg << " > " << double_4 << " = " << TF[double_4_neg > double_4] << endl;
-  cout << double_4_neg << " > " << double_5 << " = " << TF[double_4_neg > double_5] << endl;
-
-  cout << double_4_neg << " > " << double_1_neg << " = " << TF[double_4_neg > double_1_neg] << endl;
-  cout << double_4_neg << " > " << double_2_neg << " = " << TF[double_4_neg > double_2_neg] << endl;
-  cout << double_4_neg << " > " << double_3_neg << " = " << TF[double_4_neg > double_3_neg] << endl;
-  cout << double_4_neg << " > " << double_4_neg << " = " << TF[double_4_neg > double_4_neg] << endl;
-  cout << double_4_neg << " > " << double_5_neg << " = " << TF[double_4_neg > double_5_neg] << endl;
-
-  cout << endl;
-  cout << double_5_neg << " > " << double_1 << " = " << TF[double_5_neg > double_1] << endl;
-  cout << double_5_neg << " > " << double_2 << " = " << TF[double_5_neg > double_2] << endl;
-  cout << double_5_neg << " > " << double_3 << " = " << TF[double_5_neg > double_3] << endl;
-  cout << double_5_neg << " > " << double_4 << " = " << TF[double_5_neg > double_4] << endl;
-  cout << double_5_neg << " > " << double_5 << " = " << TF[double_5_neg > double_5] << endl;
-
-  cout << double_5_neg << " > " << double_1_neg << " = " << TF[double_5_neg > double_1_neg] << endl;
-  cout << double_5_neg << " > " << double_2_neg << " = " << TF[double_5_neg > double_2_neg] << endl;
-  cout << double_5_neg << " > " << double_3_neg << " = " << TF[double_5_neg > double_3_neg] << endl;
-  cout << double_5_neg << " > " << double_4_neg << " = " << TF[double_5_neg > double_4_neg] << endl;
-  cout << double_5_neg << " > " << double_5_neg << " = " << TF[double_5_neg > double_5_neg] << endl;
-  return 0;
+int main (int argc, char * argv [])
+{
+        if (argc < 2)
+                return 0;
+        ifstream input (argv[1]);
+        if (input.fail())
+                return 0;
+        cout << "Testing constructors\n";
+        cout << "\tDefault constructor: ";
+        Real dfc; cout << dfc << endl;
+        cout << "\tDouble constructor: ";
+        double D1; input >> D1;
+        Real dc1(D1); cout << dc1 << endl;
+        cout << "\tDouble constructor: ";
+        double D2; input >> D2;
+        Real dc2(D2); cout << dc2 << endl;
+        cout << "\tString constructor: ";
+        string S; input >> S;
+        Real sc(S); cout << sc << endl;
+        cout << "\tCopy constructor: ";
+        Real cc(sc); cout << cc << endl;
+        cout << "Testing assignment operator: ";
+        Real r;
+        r = sc; cout << r << endl;
+        cout << "Testing input operator:\n";
+        for (int i = 0; i < 4; i++)
+        {
+                input >> r;
+                cout << '\t' << r << endl;
+        }
+ string TF [2] = {"false", "true"};
+        string s1, s2;
+        while (input >> s1 >> s2)
+        {
+                cout << "Testing string constructor and operators\n";
+                cout << "\tInput strings: " << s1 << " and " << s2 << endl;
+                Real r1 (s1);
+                Real r2 (s2);
+                cout << "\tReal objects: " << r1 << " and " << r2 << endl;
+                cout << "\t\t" << r1 << " + " << r2 << " ==> " << r1 + r2 << endl;
+                cout << "\t\t" << r1 << " - " << r2 << " ==> " << r1 - r2 << endl;
+                cout << "\t\t" << r1 << " * " << r2 << " ==> " << r1 * r2 << endl;
+                cout << "\t\t" << r1 << " == " << r2 << " ==> " << TF[r1 == r2] << endl;
+                cout << "\t\t" << r1 << " != " << r2 << " ==> " << TF[r1 != r2] << endl;
+                cout << "\t\t" << r1 << " < " << r2 << " ==> " << TF[r1 < r2] << endl;
+                cout << "\t\t" << r1 << " <= " << r2 << " ==> " << TF[r1 <= r2] << endl;
+                cout << "\t\t" << r1 << " > " << r2 << " ==> " << TF[r1 > r2] << endl;
+                cout << "\t\t" << r1 << " >= " << r2 << " ==> " << TF[r1 >= r2] << endl;
+                cout << "\t\t" << r1 << " += " << r2; cout << " ==> " << (r1 += r2) << endl;
+                cout << "\t\t" << r1 << " -= " << r2; cout << " ==> " << (r1 -= r2) << endl;
+                cout << "\t\t" << r1 << " *= " << r2; cout << " ==> " << (r1 *= r2) << endl;
+                cout << endl;
+        }
+        input.close();
+        return 0;
 }
